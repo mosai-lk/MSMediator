7934e3ccb8b27dfa5202fc860f1b09dc19ac563a
leikai <leikai@mosainet.com>
2016-12-21 15:02:06 +0800

    test
    Signed-off-by: leikai <leikai@mosainet.com>

---
+++ Example/Pods/Target Support Files/MSMediator/MSMediator-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSMediator : NSObject
+@end
+@implementation PodsDummy_MSMediator
+@end
---
+++ MSMediator/Classes/MSMediator.h
@@ -0,0 +1,21 @@
+//
+//  MSMediator.h
+//  Pods
+//
+//  Created by LK on 2016/12/21.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MSMediator : NSObject
+
++ (instancetype)sharedInstance;
+
+// 远程App调用入口
+- (id)performActionWithUrl:(NSURL *)url completion:(void(^)(NSDictionary *info))completion;
+// 本地组件调用入口
+- (id)performTarget:(NSString *)targetName action:(NSString *)actionName params:(NSDictionary *)params shouldCacheTarget:(BOOL)shouldCacheTarget;
+- (void)releaseCachedTargetWithTargetName:(NSString *)targetName;
+
+@end
